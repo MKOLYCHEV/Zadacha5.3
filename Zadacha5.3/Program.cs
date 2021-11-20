dd203c4d9896458adeeb1ffb60e4f34fc4568480
Миша Мишин <spawn.rapture-0n@icloud.com>
2021-11-20 23:22:10 +0300

    Добавьте файлы проекта.

---
+++ Zadacha5.3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 10;
+            int[] array = new int[n];
+            Random random = new Random();
+            Console.Write("Сформированный массив: ");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(-50, 50);
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            Console.Write("Упорядоченный массив: ");
+            for (int i = 0; i < n/2; i++)
+            {
+                for (int j = i + 1; j <= n/2; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int t = array[i];
+                        array[i] = array[j];
+                        array[j] = t;
+                    }
+                }
+            }
+            for (int i = n/2; i < n-1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (array[i] < array[j])
+                    {
+                        int t = array[i];
+                        array[i] = array[j];
+                        array[j] = t;
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
